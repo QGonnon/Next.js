71379a1363e0b3559b83adcf7be37a300bfb0a19
QGonnon <Quentin.GONNON@ecole-isitech.fr>
2024-01-02 12:36:21 +0100

    init ProfileCardEdit

---
+++ my-reat-app/src/components/ProfileCardEdit/ProfileCardEdit.jsx
@@ -0,0 +1,11 @@
+import styles from './ProfileCardEdit.module.css'
+
+function ProfileCardEdit() {
+  return (
+    <>
+      <div className={styles.test}>ProfileCardEdit</div>
+    </>
+  )
+}
+
+export default ProfileCardEdit
