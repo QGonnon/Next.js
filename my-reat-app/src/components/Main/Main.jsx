90f3a3424904383e40302ccb8fbf375d20acb4dd
QGonnon <Quentin.GONNON@ecole-isitech.fr>
2024-01-02 15:16:57 +0100

    Implementation of ProfileCardEdit (form)

--- my-reat-app/src/components/Main/Main.jsx
+++ my-reat-app/src/components/Main/Main.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import styles from './Main.module.css'
+import ProfileCardEdit from '../ProfileCardEdit/ProfileCardEdit'
 
 function Main() {
   const [profileInfo, setProfileInfo] = useState({
@@ -8,15 +9,29 @@ function Main() {
     drivingLicenseCar: "non",
     drivingLicenseBike: "oui"
   })
+
+  const [editProfile,setEditProfile] = useState(false)
+
+  function handleSubmit (formData) {
+    setProfileInfo(formData)
+    setEditProfile(false)
+  };
+
+  function handleEditProfile(){
+    setEditProfile(true)
+  }
+
   return (
     <>
       <article className={styles.article}>
-        <header><div>{profileInfo.name} <br /> {profileInfo.yo}</div></header>
+        <header><div>{profileInfo.name} <br /> {profileInfo.yo} ans</div></header>
         <main>
           <div>Licence automobile : {profileInfo.drivingLicenseCar}</div>
           <div>Licence moto : {profileInfo.drivingLicenseBike}</div>
         </main>
+        <button onClick={handleEditProfile}>Edit Profile</button>
       </article>
+      {editProfile && <ProfileCardEdit profileInfo={profileInfo} childHandleSubmit={handleSubmit}/>}
     </>
   )
 }
--- my-reat-app/src/components/ProfileCardEdit/ProfileCardEdit.jsx
+++ my-reat-app/src/components/ProfileCardEdit/ProfileCardEdit.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react'
 import styles from './ProfileCardEdit.module.css'
 
-function ProfileCardEdit() {
+export default function ProfileCardEdit({profileInfo, childHandleSubmit}) {
+  const [formData, setFormData] = useState({
+    name: profileInfo.name,
+    yo: profileInfo.yo,
+    drivingLicenseCar: profileInfo.drivingLicenseCar,
+    drivingLicenseBike: profileInfo.drivingLicenseBike
+  })
+  function handleChange (event) {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+  function handleSubmit (event) {
+    event.preventDefault();
+    childHandleSubmit(formData)
+  };
+  
+
   return (
-    <>
-      <div className={styles.test}>ProfileCardEdit</div>
-    </>
+      <article className={styles.article}>
+        <form onSubmit={handleSubmit}>
+          <main>
+            <div><div>Nom : </div><input name="name" value={formData.name} onChange={handleChange} /></div>
+            <div><div>Age : </div><input name="yo" value={formData.yo} onChange={handleChange} /></div>
+            <div><div>Licence automobile : </div><input name="drivingLicenseCar" value={formData.drivingLicenseCar} onChange={handleChange} /></div>
+            <div><div>Licence moto : </div><input name="drivingLicenseBike" value={formData.drivingLicenseBike} onChange={handleChange} /></div>
+          </main>
+          <button>Submit</button>
+        </form>
+      </article>
   )
 }
 
-export default ProfileCardEdit
