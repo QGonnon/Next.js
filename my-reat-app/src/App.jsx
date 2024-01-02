020dbaee9558a22a811d6fbbf1d215c4b381d48a
QGonnon <Quentin.GONNON@ecole-isitech.fr>
2024-01-02 11:12:39 +0100

    added Profile and using arrow function for setName

--- my-reat-app/src/App.jsx
+++ my-reat-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './App.css'
+import Profile from './components/profile';
 
 function App() {
   const [name, setName]=useState("React")
   console.log(name);
   function onClickHandler(){
     setName("Vite");
+    setName((prev)=>{return prev + " Vite"})
     console.log(name);
   }
   return (
@@ -13,6 +15,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">{name}</div>
       <button onClick={onClickHandler}>click me !</button>
+      <Profile/>
     </>
   )
 }
