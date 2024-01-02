4699b5dedfe2fd006c5f2cc28217eed6c53a5a49
QGonnon <Quentin.GONNON@ecole-isitech.fr>
2024-01-02 10:47:36 +0100

    discovering useState

--- my-reat-app/src/App.jsx
+++ my-reat-app/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [name, setName]=useState("React")
+  console.log(name);
+  function onClickHandler(){
+    setName("Vite");
+    console.log(name);
+  }
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <div className="card">{name}</div>
+      <button onClick={onClickHandler}>click me !</button>
     </>
   )
 }
