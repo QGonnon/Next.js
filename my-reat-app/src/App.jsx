0eac92c7558f2ddd9c426eb27591391170853ad3
QGonnon <Quentin.GONNON@ecole-isitech.fr>
2024-01-02 11:55:13 +0100

    put logic in propsExample

--- my-reat-app/src/App.jsx
+++ my-reat-app/src/App.jsx
@@ -1,21 +1,24 @@
 import { useState } from 'react';
 import './App.css'
 import Profile from './components/Profile/Profile';
+import PropsExemple from './components/PropsExemple/PropsExemple';
 
 function App() {
   const [name, setName]=useState("React")
   console.log(name);
-  function onClickHandler(){
+  function onClickHandler(test){
     setName("Vite");
+
+    console.log("je proviens de l'enfant :", test)
+
     setName((prev)=>{return prev + " Vite"})
     console.log(name);
   }
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">{name}</div>
-      <button onClick={onClickHandler}>click me !</button>
       <Profile/>
+      <PropsExemple name={name} childClickHandler={onClickHandler}/>
     </>
   )
 }
