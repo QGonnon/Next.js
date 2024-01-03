62e4d1405fc45028969d0ed9d51c60bbb919d1da
QGonnon <Quentin.GONNON@ecole-isitech.fr>
2024-01-03 11:34:06 +0100

    using valid data to show

--- my-react-app-2/src/components/Profile/ProfileForm.jsx
+++ my-react-app-2/src/components/Profile/ProfileForm.jsx
@@ -1,36 +1,33 @@
-import styles from './ProfileList.module.css'
+import styles from './ProfileForm.module.css'
 import React, { useState } from 'react'
 
 export default function ProfileForm() {
-    const [profileInfo, setProfileInfo] = useState({
-        name: 'Quentin GONNON',
-        yo : "20",
-        drivingLicenseCar: "non",
-        drivingLicenseBike: "oui"
+      const [formData, setFormData] = useState({
+        first_name: "",
+        last_name: "",
+        email: ""
       })
-    
-      const [editProfile,setEditProfile] = useState(false)
-    
-      function handleSubmit (formData) {
-        setProfileInfo(formData)
-        setEditProfile(false)
+
+      function handleChange (event) {
+        const { name, value } = event.target;
+        setFormData((prevState) => ({ ...prevState, [name]: value }));
       };
-    
-      function handleEditProfile(){
-        setEditProfile(true)
-      }
+      function handleSubmit (event) {
+        event.preventDefault();
+        childHandleSubmit(formData)
+      };
+      
     
       return (
-        <>
           <article className={styles.article}>
-            <header><div>{profileInfo.name} <br /> {profileInfo.yo} ans</div></header>
-            <main>
-              <div>Licence automobile : {profileInfo.drivingLicenseCar}</div>
-              <div>Licence moto : {profileInfo.drivingLicenseBike}</div>
-            </main>
-            <button onClick={handleEditProfile}>Edit Profile</button>
+            <form onSubmit={handleSubmit}>
+              <main>
+                <div><div>Prénom : </div><input name="first_name" value={formData.first_name} onChange={handleChange} /></div>
+                <div><div>Nom : </div><input name="last_name" value={formData.last_name} onChange={handleChange} /></div>
+                <div><div>email : </div><input name="email" value={formData.email} onChange={handleChange} /></div>
+              </main>
+              <button>Submit</button>
+            </form>
           </article>
-          {/*editProfile && <ProfileCardEdit profileInfo={profileInfo} childHandleSubmit={handleSubmit}/>*/}
-        </>
       )
 }
